df021c2895f3ab29aa709d88cddadfa15c3cac8c
Asadullohbek05 <ayubovasadbek33@gmail.com>
2024-08-15 18:09:46 +0500

    refactoring

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import translationEN from "./locales/en/translation.json";
-import translationUZ from "./locales/uz/translation.json";
-import translationRU from "./locales/ru/translation.json";
+import translationEN from "./languages/en/translation.json";
+import translationUZ from "./languages/uz/translation.json";
+import translationRU from "./languages/ru/translation.json";
 
 i18n.use(initReactI18next).init({
   resources: {
