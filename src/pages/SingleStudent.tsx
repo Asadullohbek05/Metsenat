5bc851c06f10446ab3dc45440f10d9780bd69002
Asadullohbek05 <ayubovasadbek33@gmail.com>
2024-08-15 09:26:21 +0500

    commited

--- src/pages/SingleStudent.tsx
+++ src/pages/SingleStudent.tsx
@@ -1,5 +1,5 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { toast } from "react-toastify";
 import { useTranslation } from "react-i18next";
@@ -9,9 +9,13 @@ import arrow from "../assets/images/svg/arrow-left.svg";
 import logo from "../assets/images/svg/admin-page-logo.svg";
 import editIcon from "../assets/images/svg/edit-icon.svg";
 import sponsorIcon from "../assets/images/svg/sponsor-icon.svg";
+import request from "../server/request";
+import formatNumberWithSpaces from "../utils";
 // import saveIcon from "../assets/images/svg/save-icon.svg";
 
 const SingleStudent = () => {
+  const [studentDetails, setStudentDetails] = useState([]);
+
   const { t, i18n } = useTranslation();
   const navigate = useNavigate();
   const { setIsAuthenticated } = useContext(AuthContext) || {
@@ -25,12 +29,32 @@ const SingleStudent = () => {
   };
 
   useEffect(() => {
+    window.scrollTo(0, 0);
     const storedLang = localStorage.getItem("selectedLanguage");
     if (storedLang) {
       const { locale } = JSON.parse(storedLang);
       i18n.changeLanguage(locale);
     }
   }, [i18n]);
+
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    const getStudentDetails = async () => {
+      try {
+        const { data } = await request.get(`/student-detail/${id}`);
+        console.log(data);
+        setStudentDetails(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getStudentDetails();
+  }, [id]);
+
+  console.log(studentDetails);
+
   return (
     <div>
       <div className="bg-white">
@@ -61,12 +85,12 @@ const SingleStudent = () => {
               <img src={arrow} alt="Back" />
             </Link>
             <h3 className="text-[#28293D] font-SfProDisplay font-bold text-2xl ml-4 mr-3">
-              Isfandiyorov Iqbol Bobomirzayevich
+              {studentDetails.full_name}
             </h3>
           </div>
           <button className="px-8 py-2 bg-[#EDF1FD] text-[#3365FC] font-SfProDisplay font-medium rounded-md my-7 flex items-center gap-[10px]">
             <i className="icon-plus text-2xl"></i>
-            Homiy qo'shish
+            {t("addSponsor")}
           </button>
         </div>
       </div>
@@ -74,7 +98,7 @@ const SingleStudent = () => {
         <div className="my-10 bg-white w-[793px] mx-auto rounded-xl p-8">
           <div className="flex justify-between items-center mb-[26px]">
             <h1 className="text-[#28293D] font-SfProDisplay text-2xl font-bold">
-              Talaba haqida
+              {t("aboutStudent")}
             </h1>
             <button className="flex justify-center items-center gap-[10px] px-8 h-11 bg-[#EDF1FD] text-[#3365FC] font-SfProDisplay font-semibold tracking-[1px] rounded-md">
               <img src={editIcon} alt="Edit Icon" /> {t("edit")}
@@ -82,7 +106,7 @@ const SingleStudent = () => {
           </div>
           <div className="flex items-center">
             <span className="px-3 py-0.5 bg-[#E5EBFF] uppercase tracking-[1.13px] text-xs font-SfProDisplay font-medium text-[#3366FF]">
-              Asosiy ma'lumotlar
+              {t("mainInfo")}
             </span>
             <hr className="flex-1 bg-[#E4E8F0]" />
           </div>
@@ -91,7 +115,7 @@ const SingleStudent = () => {
               <img src={sponsorIcon} alt="Sponsor Icon" />
             </div>
             <h2 className="max-w-40 font-SfProDisplay text-[#212121] font-semibold text-[16px] tracking-[-1%]">
-              Isfandiyorov Iqbol Bobomirzayevich
+              {studentDetails.full_name}
             </h2>
           </div>
           <div className="flex gap-56">
@@ -100,13 +124,13 @@ const SingleStudent = () => {
                 {t("phoneNumber")}
               </p>
               <h3 className="text-[#212121] font-SfProDisplay text-[16px] font-semibold mt-3">
-                +998 99 973-72-60
+                {studentDetails.phone}
               </h3>
             </div>
           </div>
           <div className="flex items-center mt-8">
             <span className="px-3 py-0.5 bg-[#E5EBFF] uppercase tracking-[1.13px] text-xs font-SfProDisplay font-medium text-[#3366FF]">
-              O'qish joyi haqida ma'lumot
+              {t("infoAboutOTM")}
             </span>
             <hr className="flex-1 bg-[#E4E8F0]" />
           </div>
@@ -116,7 +140,7 @@ const SingleStudent = () => {
                 {t("otm")}
               </p>
               <h3 className="text-[#212121] font-SfProDisplay text-[16px] font-semibold mt-3">
-                O'zbekiston davlat jahon tillari universiteti
+                {studentDetails?.institute?.name}
               </h3>
             </div>
             <div className="mt-6">
@@ -124,17 +148,23 @@ const SingleStudent = () => {
                 {t("StudentType")}
               </p>
               <h3 className="text-[#212121] font-SfProDisplay text-[16px] font-semibold mt-3">
-                Bakalavr
+                {studentDetails.type === 1
+                  ? t("bachelor")
+                  : studentDetails.type === 2
+                  ? t("masterDegree")
+                  : studentDetails.type === 3
+                  ? "phd"
+                  : null}
               </h3>
             </div>
           </div>
-          <div className="flex gap-[242px]">
+          <div className="flex gap-10">
             <div className="mt-6">
-              <p className="text-[#B5B5C3] uppercase font-SfProDisplay tracking-[1.13px] text-xs">
-                Ajratilingan summa
+              <p className="text-[#B5B5C3] w-[350px] uppercase font-SfProDisplay tracking-[1.13px] text-xs">
+                {t("AllocatedAmount")}
               </p>
               <h3 className="text-[#212121] font-SfProDisplay text-[16px] font-semibold mt-3">
-                14 000 000 UZS
+                {formatNumberWithSpaces(studentDetails.given)} UZS
               </h3>
             </div>
             <div className="mt-6">
@@ -142,18 +172,18 @@ const SingleStudent = () => {
                 {t("ContractAmount")}
               </p>
               <h3 className="text-[#212121] font-SfProDisplay text-[16px] font-semibold mt-3">
-                30 000 000 UZS
+                {formatNumberWithSpaces(studentDetails.contract)} UZS
               </h3>
             </div>
           </div>
         </div>
         <div className="bg-white w-[793px] mx-auto rounded-xl p-8 h-[90px] flex justify-between items-center">
           <h2 className="text-[#28293D] font-SfProDisplay text-2xl font-bold">
-            Talabaga homiylar
+            {t("StudentSponsors")}
           </h2>
           <button className="px-8 py-2 bg-[#EDF1FD] text-[#3365FC] font-SfProDisplay font-medium rounded-md my-7 flex items-center gap-[10px]">
             <i className="icon-plus text-2xl"></i>
-            Homiy qo'shish
+            {t("addSponsor")}
           </button>
         </div>
       </div>
