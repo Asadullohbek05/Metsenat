6bfb3989cd463f127c0e8f1b6efc4f651accb393
Asadullohbek05 <ayubovasadbek33@gmail.com>
2024-08-21 09:58:45 +0500

    Language and Chart bugs fixed

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -1,132 +1,115 @@
-import { useState, useEffect } from "react";
+import { useMemo } from "react";
 import Chart from "react-apexcharts";
 import { ApexOptions } from "apexcharts";
 import { useTranslation } from "react-i18next";
 
 const ChartComponent = () => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
 
-  const [series, setSeries] = useState([
-    {
-      name: t("students"),
-      data: [10000, 20000, 8000, 30000, 15000, 20000, 25000, 35000],
-    },
-    {
-      name: t("sponsors"),
-      data: [5000, 2000, 20000, 15000, 25000, 40000, 22000, 10000],
-    },
-  ]);
-
-  const [options, setOptions] = useState<ApexOptions>({
-    chart: {
-      id: "basic-bar",
-      height: 402,
-      type: "line",
-      dropShadow: {
-        enabled: false,
-        color: "#000",
-        top: 18,
-        left: 7,
-        blur: 10,
-        opacity: 0.2,
-      },
-      zoom: {
-        enabled: false,
-      },
-      toolbar: {
-        show: false,
-      },
-    },
-    colors: ["#FF92AE", "#4C6FFF"],
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    grid: {
-      borderColor: "#e7e7e7",
-      row: {
-        opacity: 0.5,
-      },
-    },
-    markers: {
-      size: 0,
-    },
-    xaxis: {
-      categories: [
-        "Yanvar",
-        "Fevral",
-        "Mart",
-        "Aprel",
-        "May",
-        "Iyun",
-        "Iyul",
-        "Avgust",
-        "Sentabr",
-        "Oktabr",
-        "Noyabr",
-        "Dekabr",
-      ],
-    },
-    yaxis: {
-      min: 1,
-      max: 50000,
-    },
-    legend: {
-      position: "top",
-      horizontalAlign: "right",
-      floating: false,
-      offsetY: -25,
-      offsetX: -5,
-    },
-    tooltip: {
-      theme: "light",
-      x: {
-        show: true,
-        format: "MMM dd",
-      },
-      y: {
-        formatter: (value) => `${value}`,
-        title: {
-          formatter: (seriesName) => `${seriesName}:`,
-        },
-      },
-      marker: {
-        show: true,
-      },
-      style: {
-        fontSize: "12px",
-        fontFamily: "Rubik, sans-serif",
-      },
-    },
-  });
-
-  useEffect(() => {
-    setSeries([
+  const series = useMemo(
+    () => [
       {
         name: t("students"),
-        data: [10000, 20000, 8000, 30000, 15000, 20000, 25000, 35000],
+        data: [15000, 15000, 13000, 35000, 20000, 25000, 30000, 40000],
       },
       {
         name: t("sponsors"),
-        data: [5000, 2000, 20000, 15000, 25000, 40000, 22000, 10000],
+        data: [10000, 7000, 25000, 20000, 30000, 45000, 27000, 15000],
       },
-    ]);
+    ],
+    [t]
+  );
 
-    setOptions((prevOptions) => ({
-      ...prevOptions,
-      tooltip: {
-        ...prevOptions.tooltip,
-        title: {
-          formatter: (seriesName: string) => `${t(seriesName)}:`,
+  const options: ApexOptions = useMemo(
+    () => ({
+      chart: {
+        id: "basic-bar",
+        height: 402,
+        type: "line",
+        dropShadow: {
+          enabled: false,
+          color: "#000",
+          top: 18,
+          left: 7,
+          blur: 10,
+          opacity: 0.2,
+        },
+        zoom: {
+          enabled: false,
+        },
+        toolbar: {
+          show: false,
         },
       },
-    }));
-  }, [i18n.language, t]);
+      colors: ["#FF92AE", "#4C6FFF"],
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      grid: {
+        borderColor: "#e7e7e7",
+        row: {
+          opacity: 0.5,
+        },
+      },
+      markers: {
+        size: 0,
+      },
+      xaxis: {
+        categories: [
+          t("January"),
+          t("February"),
+          t("March"),
+          t("April"),
+          t("May"),
+          t("June"),
+          t("July"),
+          t("August"),
+          t("September"),
+          t("October"),
+          t("November"),
+          t("December"),
+        ],
+      },
+      yaxis: {
+        min: 1,
+        max: 50000,
+      },
+      legend: {
+        position: "top",
+        horizontalAlign: "right",
+        floating: false,
+        offsetY: -25,
+        offsetX: -5,
+      },
+      tooltip: {
+        theme: "light",
+        x: {
+          show: true,
+          format: "MMM dd",
+        },
+        y: {
+          formatter: (value) => `${value}`,
+          title: {
+            formatter: (seriesName) => `${t(seriesName)}:`,
+          },
+        },
+        marker: {
+          show: true,
+        },
+        style: {
+          fontSize: "12px",
+          fontFamily: "Rubik, sans-serif",
+        },
+      },
+    }),
+    [t]
+  );
 
   return (
-    <div className="max-w-7xl mx-auto px-10 ">
+    <div className="max-w-7xl mx-auto px-10">
       <div className="bg-white rounded-lg">
         <div className="pl-6 pt-6">
           <h1 className="text-2xl mb-2 font-medium">
@@ -134,11 +117,11 @@ const ChartComponent = () => {
           </h1>
           <div className="flex gap-5 items-center">
             <h5 className="font-rubik font-normal text-xs flex items-center gap-2 text-[#7A7A9D]">
-              <span className="w-2 h-2  rounded-full bg-[#4C6FFF]"></span>
+              <span className="w-2 h-2 rounded-full bg-[#4C6FFF]"></span>
               {t("sponsors")}
             </h5>
             <h5 className="font-rubik font-normal text-xs flex items-center gap-2 text-[#7A7A9D]">
-              <span className="w-2 h-2  rounded-full bg-[#FF92AE]"></span>
+              <span className="w-2 h-2 rounded-full bg-[#FF92AE]"></span>
               {t("students")}
             </h5>
           </div>
--- src/i18n.ts
+++ src/i18n.ts
@@ -4,13 +4,16 @@ import translationEN from "./languages/en/translation.json";
 import translationUZ from "./languages/uz/translation.json";
 import translationRU from "./languages/ru/translation.json";
 
+const savedLanguage = localStorage.getItem("selectedLanguage");
+const initialLanguage = savedLanguage ? JSON.parse(savedLanguage).locale : "en";
+
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: translationEN },
     uz: { translation: translationUZ },
     ru: { translation: translationRU },
   },
-  lng: "en",
+  lng: initialLanguage,
   fallbackLng: "en",
   interpolation: {
     escapeValue: false,
